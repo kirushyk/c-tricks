33ac996714ac56a389802a3983aa83a02d39b196
Kyrylo V. Polezhaiev <kirushyk@gmail.com>
2025-01-28 23:49:59 +0100

    next-power-of-2: Add naive and fancy smart way

---
+++ next-power-of-2.c
@@ -0,0 +1,38 @@
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+uint32_t
+naive_np2 (uint32_t n)
+{
+  uint32_t p = 1;
+  while (p < n)
+    p *= 2;
+  return p;
+}
+
+uint32_t
+np2 (uint32_t n)
+{
+  if (n == 0)
+    return 1;
+  n--;
+  n |= n >> 1;
+  n |= n >> 2;
+  n |= n >> 4;
+  n |= n >> 8;
+  n |= n >> 16;
+  return n + 1;
+}
+
+int
+main (int argc, char *argv[])
+{
+  assert (naive_np2 (0) == 1);
+  assert (np2 (0) == 1);
+  assert (naive_np2 (1) == 1);
+  assert (np2 (1) == 1);
+  assert (naive_np2 (2) == 2);
+  assert (np2 (2) == 2);
+  return EXIT_SUCCESS;
+}
