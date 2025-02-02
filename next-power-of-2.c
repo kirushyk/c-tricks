8cd7b3beca92ddd3174c2988e3097e37a9f92a4d
Kyrylo V. Polezhaiev <kirushyk@gmail.com>
2025-02-02 11:45:16 +0100

    next-power-of-2: Rename function, add range test

--- next-power-of-2.c
+++ next-power-of-2.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 uint32_t
-naive_np2 (uint32_t n)
+next_power_of_2_naive (uint32_t n)
 {
   uint32_t p = 1;
   while (p < n)
@@ -12,7 +12,7 @@ naive_np2 (uint32_t n)
 }
 
 uint32_t
-np2 (uint32_t n)
+next_power_of_2 (uint32_t n)
 {
   if (n == 0)
     return 1;
@@ -28,19 +28,22 @@ np2 (uint32_t n)
 int
 main (int argc, char *argv[])
 {
-  assert (naive_np2 (0) == 1);
-  assert (np2 (0) == 1);
-  assert (naive_np2 (1) == 1);
-  assert (np2 (1) == 1);
-  assert (naive_np2 (2) == 2);
-  assert (np2 (2) == 2);
-  assert (naive_np2 (500) == 512);
-  assert (np2 (500) == 512);
-  assert (naive_np2 (600) == 1024);
-  assert (np2 (600) == 1024);
-  assert (naive_np2 (65535) == 65536);
-  assert (np2 (65535) == 65536);
-  assert (naive_np2 (65536) == 65536);
-  assert (np2 (65536) == 65536);
+  assert (next_power_of_2_naive (0) == 1);
+  assert (next_power_of_2 (0) == 1);
+  assert (next_power_of_2_naive (1) == 1);
+  assert (next_power_of_2 (1) == 1);
+  assert (next_power_of_2_naive (2) == 2);
+  assert (next_power_of_2 (2) == 2);
+  assert (next_power_of_2_naive (500) == 512);
+  assert (next_power_of_2 (500) == 512);
+  assert (next_power_of_2_naive (600) == 1024);
+  assert (next_power_of_2 (600) == 1024);
+  assert (next_power_of_2_naive (65535) == 65536);
+  assert (next_power_of_2 (65535) == 65536);
+  assert (next_power_of_2_naive (65536) == 65536);
+  assert (next_power_of_2 (65536) == 65536);
+  for (uint32_t i = 0; i < 10000; i++) {
+    assert (next_power_of_2_naive (i) == next_power_of_2 (i));
+  }
   return EXIT_SUCCESS;
 }
